5902fd883c6882b39c8c430a6cc91f326ad854d9
weijiale <1243663286@qq.com>
2025-02-22 23:22:54 +0800

    构建框架

---
+++ wjl-sso/src/main/java/me/weijiale/wjlsso/WjlSsoApplication.java
@@ -0,0 +1,13 @@
+package me.weijiale.wjlsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjlSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjlSsoApplication.class, args);
+	}
+
+}
---
+++ wjl-sso/src/test/java/me/weijiale/wjlsso/WjlSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weijiale.wjlsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjlSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
